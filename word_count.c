3d4e4e9f70ac9693e24273c2141d4e90ba42927f
A1ec-0dell <aqodell@calpoly.edu>
2024-04-21 12:27:08 -0700

    Solved issue with consecutive spaces

--- word_count.c
+++ word_count.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    int space_count = 0, line_count = 0, byte_count = 0, character;
+    int word_count = 1, line_count = 0, byte_count = 0, character, prev_character = 'a';
 
 
     FILE *file = stdin;
@@ -21,24 +21,25 @@ int main(int argc, char *argv[])
             printf("Error opening file '%s'\n", argv[1]);
             return 1;
         }
-    } else {
-        space_count++;
     }
 
     while ((character = fgetc(file)) != EOF)
     {
         if (isspace(character))
         {
-            space_count++;
+            if(!isspace(prev_character)){
+                word_count++;
+            }
         }
         if (character == '\n')
         {
             line_count++;
         }
         byte_count = byte_count + sizeof(char);
+        prev_character = character;
     }
 
     fclose(file);
-    printf("%d %d %d\n",  line_count, space_count, byte_count);
+    printf("%d %d %d\n",  line_count, word_count, byte_count);
     return 0;
 }
