d515c36d51856950d5def265943e24fc78ebf26a
A1ec-0dell <aqodell@calpoly.edu>
2024-04-21 12:16:41 -0700

    implements word_count with stdin

--- word_count.c
+++ word_count.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    int space_count = 0;
-    int line_count = 0;
-    int character;
-    FILE *file;
-    if (argc != 2)
+    int space_count = 0, line_count = 0, byte_count = 0, character;
+
+
+    FILE *file = stdin;
+
+    if (argc > 2)
     {
-        printf("Usage: %s <filename>\n", argv[0]);
+        printf("Usage: %s [filename (optional)]\n", argv[0]);
         return 1;
     }
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        printf("Error opening file '%s'\n", argv[1]);
-        return 1;
+
+    if(argc == 2){
+        file = fopen(argv[1], "r");
+        if(!file){
+            printf("Error opening file '%s'\n", argv[1]);
+            return 1;
+        }
+    } else {
+        space_count++;
     }
 
     while ((character = fgetc(file)) != EOF)
@@ -24,12 +31,14 @@ int main(int argc, char *argv[])
         {
             space_count++;
         }
-        if (character == '\n'){
+        if (character == '\n')
+        {
             line_count++;
         }
+        byte_count = byte_count + sizeof(char);
     }
 
     fclose(file);
-    printf("%d %d", space_count, line_count);
+    printf("%d %d %d\n",  line_count, space_count, byte_count);
     return 0;
 }
